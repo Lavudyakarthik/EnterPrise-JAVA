b84487b7870827e013ae858e159b63d3abe1b837
Lavudyakarthik <100749349+Lavudyakarthik@users.noreply.github.com>
2022-08-19 15:55:02 +0530

    Add files via upload

---
+++ Assignment1Q2.java
@@ -0,0 +1,16 @@
+package com.karthik;
+
+public class Assignment1Q2 {
+	
+	public static void main(String[] args) {
+		// Question 2
+		int a=5;
+		for(int i=1;i<a;i++) {
+			for(int j=1;j<a;j++) {
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+	}
+
+}
