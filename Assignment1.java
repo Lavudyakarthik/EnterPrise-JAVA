be330f02266f4d97a546f0afbefaa5ae7c1b01f7
Lavudyakarthik <100749349+Lavudyakarthik@users.noreply.github.com>
2022-08-19 15:49:34 +0530

    Add files via upload

--- Assignment1.java
+++ Assignment1.java
@@ -3,15 +3,18 @@ package com.karthik;
 public class Assignment1 {
 	
 	public static void main(String[] args) {
-		// Question 2
-	int a = 5;
-	for(int i=1;i<a;i++) {
-		for(int j=1;j<a;j++) {
-			System.out.print(i);
+		// Question 3
+		int n=14;
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				if(i==0 || j==0 || i==n-1 || j==n-1 || i+j<=(n-1)/2 || j-i>=(n-1)/2)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
 		}
-		System.out.println();
-	  }
-
+	
 	}
 
 }
