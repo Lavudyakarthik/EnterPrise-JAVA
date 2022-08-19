4e2b296f4e5c756973c5c3f06f3fe10c368403e0
Lavudyakarthik <100749349+Lavudyakarthik@users.noreply.github.com>
2022-08-19 16:15:17 +0530

    Update Assignment1Q5.java

--- Assignment1Q5.java
+++ Assignment1Q5.java
@@ -5,9 +5,9 @@ public class Assignment1Q5 {
 	public static void main(String[] args) {
 		// Question 5
 		int n=14;
-		for(int i=1;i<n;i++) {
-			for(int j=1;j<n;j++) {
-				if(i==1||j==1||i==n-1||i-j>=(n)/2||i+j<=(n)/2)
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				if(i==1||j==1||i==n-1||i-j>=(n-1)/2||i+j<=(n-1)/2)
 				System.out.print("*");
 				else
 					System.out.print(" ");
