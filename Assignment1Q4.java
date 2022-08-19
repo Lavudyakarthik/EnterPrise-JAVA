b0575f5c4ba3569c20b51095382cd6d7cc2a58fd
Lavudyakarthik <100749349+Lavudyakarthik@users.noreply.github.com>
2022-08-19 16:14:19 +0530

    Update Assignment1Q4.java

--- Assignment1Q4.java
+++ Assignment1Q4.java
@@ -5,8 +5,8 @@ public class Assignment1Q4 {
 	public static void main(String[] args) {
 		// Question 4
 		int n=14;
-		for(int i=1;i<n;i++) {
-			for(int j=1;j<n;j++) {
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
 				if(i-j>=(n-1)/2||i+j>=n-1+n/2)
 				System.out.print("*");
 				else
